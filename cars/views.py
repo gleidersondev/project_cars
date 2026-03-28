2769847a7811adf9dfaf90a678edfbf9fabcc04b
Gleiderson <gleidersondev@gmail.com>
2026-03-28 14:18:11 -0300

    Ordenando resultado de busca pelo usuario

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_view(request):
+  cars = Car.objects.all().order_by('model')
+  search = request.GET.get('search')
+  
+  if search:
+    cars = Car.objects.filter(model__contains=search)
+  
+  return render (
+    request,
+    'cars.html',
+    {'cars': cars}
+  )
+
